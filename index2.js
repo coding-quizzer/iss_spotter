f702f6d8287d34b7cb792e7cdd04c9e55312bd5d
William Gadd <wgadd@sasktel.net>
2022-09-30 02:32:28 +0000

    Reimplimented all iss_promise functions using promises

--- index2.js
+++ index2.js
@@ -1,5 +1,14 @@
-const { fetchMyIP } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then((body) => console.log(body))
-  .catch((err) => console.log(err));
+nextISSTimesForMyLocation().
+then( body => {
+  passTimes = body.response;
+  for (let element of passTimes) {
+    const risetime = element.risetime;
+    const duration = element.duration;
+    const date = Date(risetime);
+    console.log(`Next pass at ${date} for ${duration} seconds!`);  
+  }
+  
+})
+.catch((error) => console.log("it didn't work: ", error.message));
--- iss_promised.js
+++ iss_promised.js
@@ -4,6 +4,25 @@ const fetchMyIP = function() {
 
   return request('https://api.ipify.org/?format=json');
 
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://ipwho.is/${ip}`);
+};
+
+const fetchFlyoverTimesForISS = function(body) {
+  bodyObj = JSON.parse(body);
+  const long = bodyObj.longitude;
+  const lat = bodyObj.latitude;
+  return request(`https://iss-flyover.herokuapp.com/json/?lat=${lat}&lon=${long}`)
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchFlyoverTimesForISS)
+  .then(body => JSON.parse(body))
 }
 
-module.exports = {fetchMyIP};
+module.exports = {nextISSTimesForMyLocation};
